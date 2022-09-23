4eb34fbef1271e90dff0aec04fd714f3b2306f2a
Krunal Bhandekar <krunalbhandekar10@gmail.com>
2022-09-23 13:56:43 +0530

    mock2

---
+++ mock2/index.js
@@ -0,0 +1,450 @@
+let jobdata=[
+{
+isNew: "true",
+company: "Ellette",
+postedAt: "2021-03-30",
+city: "Gangtok",
+location: "American Samoa",
+countryCode: "WF",
+role: "Frontend",
+level: "Junior",
+contract: "Full Time",
+position: " Backend Developer",
+language: "Java"
+},
+{
+"isNew": "false",
+"company": "Jinny",
+"postedAt": "2021-09-09",
+"city": "Ludhiana",
+"location": "Belgium",
+"countryCode": "MU",
+"role": "FullStack",
+"level": "Junior",
+"contract": "Full Time",
+"position": "Junior Frontend Developer",
+"language": "Java"
+},
+{
+"isNew": "false",
+"company": "Gale",
+"postedAt": "2021-10-04",
+"city": "Port Said",
+"location": "Tanzania, United Republic of",
+"countryCode": "BH",
+"role": "FullStack",
+"level": "Senior",
+"contract": "Full Time",
+"position": " Backend Developer",
+"language": "Java"
+},
+{
+"isNew": "false",
+"company": "Amalie",
+"postedAt": "2021-09-02",
+"city": "Lomé",
+"location": "Fiji",
+"countryCode": "SL",
+"role": "Frontend",
+"level": "Senior",
+"contract": "Full Time",
+"position": "Junior Frontend Developer",
+"language": "Javascript"
+},
+{
+"isNew": "true",
+"company": "Gertrud",
+"postedAt": "2021-03-15",
+"city": "Baku",
+"location": "Fiji",
+"countryCode": "GI",
+"role": "Frontend",
+"level": "Fresher",
+"contract": "Part Time",
+"position": "Senior Frontend Developer",
+"language": "HTML"
+},
+{
+"isNew": "false",
+"company": "Jenda",
+"postedAt": "2021-03-25",
+"city": "Dalian",
+"location": "Czech Republic",
+"countryCode": "CV",
+"role": "FullStack",
+"level": "Junior",
+"contract": "Full Time",
+"position": "Senior Frontend Developer",
+"language": "Ruby"
+},
+{
+"isNew": "false",
+"company": "Aryn",
+"postedAt": "2021-08-24",
+"city": "Riga",
+"location": "Nepal",
+"countryCode": "UA",
+"role": "Frontend",
+"level": "Junior",
+"contract": "Part Time",
+"position": "Junior Frontend Developer",
+"language": "Javascript"
+},
+{
+"isNew": "false",
+"company": "Wendi",
+"postedAt": "2021-11-19",
+"city": "Kuala Lumpur",
+"location": "Trinidad and Tobago",
+"countryCode": "SK",
+"role": "FullStack",
+"level": "Junior",
+"contract": "Part Time",
+"position": "Junior Frontend Developer",
+"language": "Java"
+},
+{
+"isNew": "true",
+"company": "Gabriellia",
+"postedAt": "2021-11-12",
+"city": "Hilo",
+"location": "Croatia",
+"countryCode": "IL",
+"role": "FullStack",
+"level": "Fresher",
+"contract": "Full Time",
+"position": " Backend Developer",
+"language": "Java"
+},
+{
+"isNew": "true",
+"company": "Danika",
+"postedAt": "2021-01-14",
+"city": "N'Djamena",
+"location": "Italy",
+"countryCode": "ID",
+"role": "FullStack",
+"level": "Junior",
+"contract": "Part Time",
+"position": "Senior Frontend Developer",
+"language": "Ruby"
+},
+{
+"isNew": "true",
+"company": "Valera",
+"postedAt": "2021-02-28",
+"city": "Rio Branco",
+"location": "United Kingdom",
+"countryCode": "IL",
+"role": "Frontend",
+"level": "Senior",
+"contract": "Part Time",
+"position": "Senior Frontend Developer",
+"language": "React"
+},
+{
+"isNew": "true",
+"company": "Teriann",
+"postedAt": "2021-06-07",
+"city": "Tampa",
+"location": "South Africa",
+"countryCode": "LV",
+"role": "FullStack",
+"level": "Senior",
+"contract": "Part Time",
+"position": "Junior Frontend Developer",
+"language": "Ruby"
+},
+{
+"isNew": "false",
+"company": "Ardeen",
+"postedAt": "2022-01-03",
+"city": "Split (city)",
+"location": "Holy See (Vatican City State)",
+"countryCode": "GU",
+"role": "FullStack",
+"level": "Fresher",
+"contract": "Part Time",
+"position": "Software Engineer",
+"language": "Ruby"
+},
+{
+"isNew": "false",
+"company": "Mahalia",
+"postedAt": "2021-11-27",
+"city": "Ciudad del Este",
+"location": "Tunisia",
+"countryCode": "ML",
+"role": "FullStack",
+"level": "Junior",
+"contract": "Full Time",
+"position": "Software Engineer",
+"language": "Java"
+},
+{
+"isNew": "true",
+"company": "Brietta",
+"postedAt": "2021-11-13",
+"city": "Ilhéus",
+"location": "Northern Mariana Islands",
+"countryCode": "CI",
+"role": "Frontend",
+"level": "Junior",
+"contract": "Part Time",
+"position": "Junior Frontend Developer",
+"language": "Ruby"
+},
+{
+"isNew": "true",
+"company": "Iseabal",
+"postedAt": "2021-11-17",
+"city": "Arbil",
+"location": "Aruba",
+"countryCode": "IE",
+"role": "FullStack",
+"level": "Junior",
+"contract": "Full Time",
+"position": "Junior Frontend Developer",
+"language": "Java"
+},
+{
+"isNew": "false",
+"company": "Edyth",
+"postedAt": "2021-09-27",
+"city": "Minneapolis",
+"location": "Norfolk Island",
+"countryCode": "BN",
+"role": "FullStack",
+"level": "Fresher",
+"contract": "Part Time",
+"position": "Software Engineer",
+"language": "Javascript"
+},
+{
+"isNew": "false",
+"company": "Sharlene",
+"postedAt": "2021-09-12",
+"city": "Denver",
+"location": "Saint Lucia",
+"countryCode": "YE",
+"role": "Frontend",
+"level": "Senior",
+"contract": "Part Time",
+"position": "Software Engineer",
+"language": "Java"
+},
+{
+"isNew": "true",
+"company": "Yvonne",
+"postedAt": "2021-06-09",
+"city": "Durban",
+"location": "Holy See (Vatican City State)",
+"countryCode": "AM",
+"role": "FullStack",
+"level": "Fresher",
+"contract": "Full Time",
+"position": "Software Engineer",
+"language": "Ruby"
+},
+{
+"isNew": "false",
+"company": "Luci",
+"postedAt": "2021-09-25",
+"city": "Chengdu",
+"location": "Bahrain",
+"countryCode": "SK",
+"role": "Frontend",
+"level": "Fresher",
+"contract": "Part Time",
+"position": " Backend Developer",
+"language": "HTML"
+},
+{
+"isNew": "false",
+"company": "Sabina",
+"postedAt": "2021-12-30",
+"city": "Labasa",
+"location": "Ethiopia",
+"countryCode": "RW",
+"role": "Frontend",
+"level": "Fresher",
+"contract": "Full Time",
+"position": " Backend Developer",
+"language": "React"
+},
+{
+"isNew": "true",
+"company": "Lulita",
+"postedAt": "2021-04-01",
+"city": "Mumbai",
+"location": "Solomon Islands",
+"countryCode": "SG",
+"role": "FullStack",
+"level": "Senior",
+"contract": "Part Time",
+"position": "Software Engineer",
+"language": "HTML"
+},
+{
+"isNew": "false",
+"company": "Kalina",
+"postedAt": "2022-01-11",
+"city": "Cebu City",
+"location": "Guinea-Bissau",
+"countryCode": "IT",
+"role": "FullStack",
+"level": "Junior",
+"contract": "Full Time",
+"position": "Junior Frontend Developer",
+"language": "Ruby"
+},
+{
+"isNew": "false",
+"company": "Moyna",
+"postedAt": "2021-02-25",
+"city": "Kiev",
+"location": "Benin",
+"countryCode": "RO",
+"role": "FullStack",
+"level": "Junior",
+"contract": "Part Time",
+"position": "Senior Frontend Developer",
+"language": "HTML"
+},
+{
+"isNew": "true",
+"company": "Michaelina",
+"postedAt": "2021-06-24",
+"city": "Wollongong",
+"location": "Marshall Islands",
+"countryCode": "TK",
+"role": "Frontend",
+"level": "Fresher",
+"contract": "Part Time",
+"position": "Software Engineer",
+"language": "Javascript"
+},
+{
+"isNew": "true",
+"company": "Shannah",
+"postedAt": "2021-10-27",
+"city": "Saint-Pierre",
+"location": "Slovenia",
+"countryCode": "BI",
+"role": "FullStack",
+"level": "Fresher",
+"contract": "Part Time",
+"position": " Backend Developer",
+"language": "Javascript"
+}
+]
+
+let current_page=1
+let page=document.querySelector(".page")
+let pagee=document.querySelector(".pagee")
+page.textContent=current_page
+pagee.textContent=current_page
+
+let total_pages = Math.ceil(jobdata.length / 10);
+
+function inc(){
+    if(current_page<total_pages){
+        current_page=current_page+1
+        page.textContent=current_page
+  pagee.textContent=current_page
+
+        displayData(jobdata, current_page, 10)
+        console.log('current_page:', current_page)
+    }
+    else{
+        alert("page limit over")
+    }
+}
+
+function dec(){
+    if(current_page>1){
+        current_page=current_page-1
+        page.textContent=current_page
+pagee.textContent=current_page
+
+        displayData(jobdata, current_page, 10)
+        console.log('current_page:', current_page)
+    }
+     else{
+        alert("page can't be less than 1 ")
+    }
+}
+
+window.addEventListener("load",function(){
+  displayData(jobdata, current_page, 10)
+})
+
+function displayData(jobdata, current_page, per_page_items){
+  document.querySelector("#alldata").innerHTML=""
+
+    let page = current_page || 1,
+	per_page = per_page_items || 10,
+	offset = (page - 1) * per_page,
+
+	paginatedItems = jobdata.slice(offset).slice(0, per_page_items)
+	
+    
+  paginatedItems.map(function(el){
+
+    let main = document.createElement("div")
+    main.setAttribute("class", "main");
+  let box = document.createElement("div")
+  let box1 = document.createElement("div")
+  let box2 = document.createElement("div")
+  let box3 = document.createElement("div")
+  let main2 = document.createElement("div")
+  main2.setAttribute("class", "main2")
+
+  let image=document.createElement("img")
+    image.src="https://image.shutterstock.com/image-vector/job-vacancy-sign-vector-illustration-260nw-360897173.jpg"
+    image.setAttribute("class", "img");
+  let company=document.createElement("p")
+  company.textContent=el.company
+  let postedAt = document.createElement("p")
+  postedAt.textContent=el.postedAt
+  let position = document.createElement("p")
+  position.textContent=el.position
+  let contract = document.createElement("p")
+  contract.textContent=el.contract
+  let location = document.createElement("p")
+  location.textContent=el.location
+  let level = document.createElement("p")
+  level.textContent=el.level
+  let role = document.createElement("p")
+  role.textContent=el.role
+  let language = document.createElement("p")
+  language.textContent=el.language
+  
+  box.append(image)
+  box.setAttribute("class", "imgdiv");
+  box3.append(postedAt,contract,location)
+  box3.setAttribute("class", "posted")
+  box1.append(company,position,box3)
+  box2.append(role,level,language)
+   box2.setAttribute("class", "role")
+  main2.append(box,box1)
+  main.append(main2,box2)
+  document.querySelector("#alldata").append(main)
+  })
+}
+
+function handlerole(){
+    let filterrole=document.querySelector("#sortrole").value
+    let filterdata=jobdata.filter((e)=>e.role==filterrole)
+   displayData(filterdata, current_page, 10)
+}
+
+function handlesearch(){
+    let search=document.querySelector("#search").value
+    let s=search.toLowerCase()
+    let sdata=jobdata.filter((e)=>e.language.toLowerCase()==s)
+    displayData(sdata, current_page, 10)
+}
+
+
+
